db226ba45be8e42126a079e9dc0c5afa0d3925e1
srikanth157 <srikanthkodari157@gmail.com>
2022-07-06 02:02:59 +0530

    modal updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,32 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Badge from "@mui/material/Badge";
 import Nav from "react-bootstrap/Nav";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 import Menu from "@mui/material/Menu";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Table from "react-bootstrap/esm/Table";
 import { DLT } from "../redux/actions/action";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const Header = () => {
   const [price, setPrice] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
 
   const getdata = useSelector((state) => state.cartreducer.carts);
   // console.log(getdata);
@@ -46,8 +64,8 @@ const Header = () => {
     <>
       <Navbar bg="dark" variant="dark" style={{ height: "60px" }}>
         <Container>
-          <NavLink to="/" className="text-decoration-none text-light mx-3">
-            ISMART
+          <NavLink to="/" className="text-decoration-none text-light mx-5">
+            ISMART-Cart
           </NavLink>
           <Nav className="me-auto">
             <NavLink to="/" className="text-decoration-none text-light">
@@ -134,6 +152,49 @@ const Header = () => {
                   <p className="text-center">Total :₹ {price}</p>
                 </tbody>
               </Table>
+              <Button variant="contained" color="success" onClick={handleOpen}>
+                Check out
+              </Button>
+              <Modal
+                open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    CONGRATULATIONS
+                  </Typography>
+                  <img
+                    src="./congo.gif"
+                    alt=""
+                    className="emptycart_img"
+                    style={{ width: "5rem", padding: 10 }}
+                  />
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Your order placed successfully
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      marginTop: 5,
+                    }}
+                  >
+                    <Button
+                      variant="contained"
+                      onClick={(handleCloseModal, handleClose)}
+                    >
+                      Place your Next order
+                    </Button>
+                  </Box>
+                </Box>
+              </Modal>
             </div>
           ) : (
             <div
@@ -151,13 +212,7 @@ const Header = () => {
                   cursor: "pointer",
                 }}
               ></i>
-              <p style={{ fontSize: 22 }}>Your carts is empty</p>
-              <img
-                src="./cart.gif"
-                alt=""
-                className="emptycart_img"
-                style={{ width: "5rem", padding: 10 }}
-              />
+              <p style={{ fontSize: 22 }}>Your cart is empty</p>
             </div>
           )}
         </Menu>
